d7e9e658ba84f2fe5563cc1e0fbe7e72ef7d8cfe
liyong <onlineliyong@163.com>
2022-04-19 20:55:57 +0800

    amend

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("pr add content");
         System.out.println("pr +HEAD");
         System.out.println("test no --amend");
+        System.out.println("test --amend");
     }
 
 }
