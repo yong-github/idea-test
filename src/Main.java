080fe97e81cbe7bd2b9ce466822d462e45cc85bd
liyong <onlineliyong@163.com>
2022-04-21 20:33:36 +0800

    delete

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
-        System.out.println("update!");
-        System.out.println("git diff!");
-        System.out.println("pr add content");
-        System.out.println("pr +HEAD");
-        System.out.println("test no --amend");
-        System.out.println("test --amend");
+        System.out.println("HelloScala!");
     }
 
 }
