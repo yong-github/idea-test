4d9419f84dff5418ea7f65cd72e80fd22546524d
liyong <onlineliyong@163.com>
2022-04-21 20:39:20 +0800

    add 2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloScala!");
         System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 
 }
