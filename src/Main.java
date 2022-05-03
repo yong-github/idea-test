e42430ff9cefe4be230d7e86606aa71db52ec42a
young <35259202+yong-github@users.noreply.github.com>
2022-05-04 01:23:12 +0800

    Pr (#5)
    * pr 1
    
    * add pr 2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("master 1");
+        System.out.println("pr 1");
+        System.out.println("pr 2");
     }
 
 }
